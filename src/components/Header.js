ad509f5a15770e43cf07c554693c388614ed27c5
Alecs Reasonda <momai.reason@gmail.com>
2023-04-05 15:12:17 -0400

    added dependencies, added meta tags, fixed basic semantics in components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            Little Lemon Header
+        </header>
+
+    )
+}
+
+export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import Header from "./Header"
 const Home = () => {
   return (
-    <div>
-        <header>
-            Little Lemon Header
-        </header>
-    </div>
+    <>
+        <Header />
+        <main>
+            main content
+        </main>
+    </>
   )
 }
 
