22703d9676dc594670e08a21420f37704ceabc4b
Alecs Reasonda <momai.reason@gmail.com>
2023-04-05 14:59:51 -0400

    added navigation code and styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,11 @@
 const Navigation = () => {
     return (
         <nav>
-            Navigation
+            <ul>
+                <li><a href="/home" role="button">Home</a></li>
+                <li><a href="/about" role="button">About</a></li>
+                <li><a href="/reserve" role="button">Reservations</a></li>
+            </ul>
         </nav>
     )
 }
