0ab63077f9b4a91c0f2741fe5141281310aeda8a
Alecs Reasonda <momai.reason@gmail.com>
2023-04-10 14:13:19 -0400

    fixed responsiveness of navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,20 +2,38 @@ import { Link } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdAddShoppingCart } from "react-icons/md";
 import logo from '../assets/Logo.png'
+import { useState } from "react";
+import { click } from "@testing-library/user-event/dist/click";
 
 const Navigation = () => {
-    const navContentMobile = (
-        <div>
-            <RxHamburgerMenu />
-            <img src={logo} alt='little lemon logo'/>
-            <MdAddShoppingCart />
-        </div>
-    );
+    const [clicked, setClicked] = useState(false);
     const navContentDesktop = (
-        <ul>
+        <ul className='desktop'>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/reserve'>Reservations</Link></li>
         </ul>
+    );
+
+    const openedMenu = (
+        <ul className={clicked ? 'mobile-nav' : 'hidden'}>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/reserve'>Reservations</Link></li>
+        </ul>
+    )
+
+    const clickHandler = () => {
+        setClicked(!clicked)
+    }
+
+    const navContentMobile = (
+        <div className='mobile'>
+            <div className='mobile__upper'>
+                <RxHamburgerMenu onClick={clickHandler} />
+                <img src={logo} alt='little lemon logo' />
+                <MdAddShoppingCart />
+            </div>
+            {openedMenu}
+        </div>
     );
 
     return (
